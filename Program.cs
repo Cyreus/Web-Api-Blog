57c352f8a341466472b4d09cd562138f5c993e07
Muhammed Ali Öztürk <Muhammed_Ali95@outlook.com.tr>
2024-08-01 12:22:39 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Configuration;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using WebUygulamasý.Data;
+using WebUygulamasÃ½.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,15 +10,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.IdentityModel.Tokens;
-using WebUygulamasý.Helpers;
+using WebUygulamasÃ½.Helpers;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
